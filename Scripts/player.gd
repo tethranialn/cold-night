84a2a9e47a5e77b7314fd56e27b044c583d7b2b8
tethranialn <tethranialn@ya.ru>
2026-01-07 21:16:33 +0300

    Бег

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -1,22 +1,36 @@
 extends CharacterBody2D
 
-var speed = 250
+var speed = 370
+var run_speed = 1.0
+var target_run_speed = 1.0
+var is_running = false
 var player_state
 var last_foot_direction = Vector2.DOWN
 var rotation_speed = 15.0
 var current_weapon = "knife"
 var is_attacking = false
+var acceleration = 0.1
 
 func _physics_process(delta):
 	var direction = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
 	
-	if direction.x == 0 and direction.y == 0:
+	if direction.length() == 0:
+		is_running = false
 		player_state = "idle"
-	elif direction.x != 0 or direction.y != 0:
+	else:
 		player_state = "walking"
 		last_foot_direction = direction.normalized()
 	
-	velocity = direction * speed
+	if Input.is_action_pressed("run") and direction.length() > 0:
+		is_running = true
+		target_run_speed = 2.0
+	else:
+		is_running = false
+		target_run_speed = 1.0
+	
+	run_speed = lerp(run_speed, target_run_speed, acceleration)
+	
+	velocity = direction * speed * run_speed
 	move_and_slide()
 	
 	sprite_rotation(delta, direction)
@@ -47,23 +61,27 @@ func sprite_rotation(delta, direction):
 func play_anim():
 	if $body.animation == "Knife_attack" and $body.is_playing():
 		return
+	
 	if current_weapon == "flashlight":
 		$body.offset = Vector2(-93, -107)
 		$body.play("Flashlight")
-		if player_state == "idle": $feet.play("idle")
-		elif player_state == "walking": $feet.play("walk")
 	elif current_weapon == "knife":
 		$body.offset = Vector2(-105, -113)
 		$body.play("Knife_idle")
-		if player_state == "idle": $feet.play("idle")
-		elif player_state == "walking": $feet.play("walk")
+	
+	if player_state == "idle":
+		$feet.play("idle")
+	elif player_state == "walking":
+		if is_running:
+			$feet.play("run")
+		else:
+			$feet.play("walk")
 
 func switch_flashlight():
 	if current_weapon == "flashlight":
 		current_weapon = "knife"
 	else:
 		current_weapon = "flashlight"
-	
 	play_anim()
 
 func play_knife_attack():
